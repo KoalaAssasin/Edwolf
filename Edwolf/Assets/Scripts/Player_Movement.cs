045e4d2f9e193b1dbe972c9e586f38edeebf7df4
KoalaAssasin <koalaassasinn@gmail.com>
2022-10-17 01:21:25 +1100

    basic movement

---
+++ Edwolf/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    Rigidbody rigibody;
+    [SerializeField] float MoveSpeedHorizontal = 5f;
+    [SerializeField] float MoveSpeedVertical = 5f;
+    [SerializeField] float JumpVelocity = 5f;
+
+    private void Start()
+    {
+        rigibody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rigibody.velocity = new Vector3(horizontalInput * MoveSpeedHorizontal, rigibody.velocity.y, verticalInput * MoveSpeedVertical);
+
+        if (Input.GetKeyDown("space"))
+        {
+            rigibody.velocity = new Vector3(rigibody.velocity.x, JumpVelocity, rigibody.velocity.z);
+        }
+
+    }
+}
