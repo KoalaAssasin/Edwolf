4424a00004947d020d9331056a1df4361cf42ee0
KoalaAssasin <koalaassasinn@gmail.com>
2022-10-28 18:51:50 +1100

    -Melee in
    Can punch.
    Small cooldown on punch.
    Punch enemy three times to kill it.
    Enemy has IFrames.
    WASD currently doesnt work properly. Use arrow keys and z for punch and x for jump

--- Edwolf/Assets/Scripts/Basic_Enemy.cs
+++ Edwolf/Assets/Scripts/Basic_Enemy.cs
@@ -9,8 +9,12 @@ public class Basic_Enemy : MonoBehaviour
     private Vector3 wayPointPos;
     //This will be the enemy's speed. Adjust as necessary.
     public float enemySpeed = 6.0f;
-    //private int health = 3;
+    private int health = 3;
     float oldPosition;
+    float IFrames = 0.5f;
+    bool injured = false;
+
+    public Player_Movement playerScript;
 
     void Start()
     {
@@ -32,6 +36,34 @@ public class Basic_Enemy : MonoBehaviour
 
         wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
         //Here the enemies will follow the waypoint.
-        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, enemySpeed * Time.deltaTime);
+       transform.position = Vector3.MoveTowards(transform.position, wayPointPos, enemySpeed * Time.deltaTime);
+
+        if (injured == true)
+        {
+            IFrames -= Time.deltaTime;
+        }
+        if (IFrames <= 0)
+        {
+            injured = false;
+            IFrames = 0.5f;
+        }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "PlayerPunchBox")
+        {
+
+            if (playerScript.punchActive && injured == false)
+            {
+                health -= 1;
+                injured = true;
+            }
+            if (health == 0)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
     }
 }
--- Edwolf/Assets/Scripts/Player_Movement.cs
+++ Edwolf/Assets/Scripts/Player_Movement.cs
@@ -12,6 +12,13 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
 
+    public GameObject PunchBox;
+
+    public bool punchActive = false;
+    bool punchCooldown = false;
+    float punchActiveTime = 0.5f;
+    string direction = "right";
+
     private void Start()
     {
         rigibody = GetComponent<Rigidbody>();
@@ -24,9 +31,46 @@ public class Player_Movement : MonoBehaviour
 
         rigibody.velocity = new Vector3(horizontalInput * MoveSpeedHorizontal, rigibody.velocity.y, verticalInput * MoveSpeedVertical);
 
-        if (Input.GetKeyDown("space") && GroundTouch() == true)
+        if (Input.GetKeyDown("x") && GroundTouch() == true)
         {
             rigibody.velocity = new Vector3(rigibody.velocity.x, JumpVelocity, rigibody.velocity.z);
+        }
+
+        if (Input.GetKeyDown("left") && direction == "right")
+        {
+            direction = "left";
+            PunchBox.transform.position = new Vector3(PunchBox.transform.position.x - 1f, PunchBox.transform.position.y, PunchBox.transform.position.z);
+        }
+
+        if (Input.GetKeyDown("right") && direction == "left")
+        {
+            direction = "right";
+            PunchBox.transform.position = new Vector3(PunchBox.transform.position.x + 1f, PunchBox.transform.position.y, PunchBox.transform.position.z);
+        }
+
+        //Punch time
+        if (Input.GetKeyDown("z") && punchCooldown == false)
+        {
+            punchActive = true;
+            punchCooldown = true;
+        }
+        //Countdown the punch active time and cooldown
+        if (punchActiveTime > -2f && punchCooldown == true)
+        {
+            punchActiveTime -= Time.deltaTime;
+        }
+        //Deactivate after punch active time hits 0
+        if (punchActiveTime < 0f && punchActive == true)
+        {
+            //Debug.Log("punch inactive");
+            punchActive = false;
+        }
+        //Punch cooldown is done half a second after punch is active (times to be edited)
+        if (punchActiveTime < -0.5f)
+        {
+            punchCooldown = false;
+            punchActiveTime = 0.5f;
+            //Debug.Log("punch cooldowned");
         }
 
     }
@@ -35,4 +79,5 @@ public class Player_Movement : MonoBehaviour
     {
        return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
     }
+
 }
