e202be0ca99dd9f11169368ae81f671a23528ee8
KoalaAssasin <koalaassasinn@gmail.com>
2022-11-06 22:07:24 +1100

    enemies face as they chase

--- Edwolf/Assets/Scripts/Tank_Enemy.cs
+++ Edwolf/Assets/Scripts/Tank_Enemy.cs
@@ -19,6 +19,7 @@ public class Tank_Enemy : MonoBehaviour
     string patrolDirection = "up";
 
     public Player_Movement playerScript;
+    public Transform target;
 
     [SerializeField] Transform playerCheck;
     [SerializeField] LayerMask playerMask;
@@ -37,14 +38,7 @@ public class Tank_Enemy : MonoBehaviour
             //To make them stunned during IFrames
             if (injured == false)
             {
-                if (oldPosition < transform.position.x) //Sprite rotator
-                {
-                    transform.localRotation = Quaternion.Euler(0, 180, 0);
-                }
-                else
-                {
-                    transform.localRotation = Quaternion.Euler(0, 0, 0);
-                }
+                transform.LookAt(target);
                 oldPosition = transform.position.x;
 
                 wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
